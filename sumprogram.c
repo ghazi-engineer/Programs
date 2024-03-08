61bdf341489464180fc3496cc22228fd8ecf04e9
ghazi-engineer <162737023+ghazi-engineer@users.noreply.github.com>
2024-03-09 00:45:45 +0500

    Add files via upload

---
+++ sumprogram.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+    int x;
+    int y;
+
+    printf("\nEnter a number:\t");
+    scanf("%d", &x);
+
+    printf("\nEnter a second number:\t");
+    scanf("%d", &y);
+
+    printf("\nSum is\t %d", x + y);
+    return 0;
+}
