0cc0a4bab5ebc275b702ae7852dae7bc1b170481
ghazi-engineer <162737023+ghazi-engineer@users.noreply.github.com>
2024-03-11 19:42:47 +0500

    Add files via upload

---
+++ program.c
@@ -0,0 +1,44 @@
+ #include<stdio.h>
+
+
+/*
+mera naam ghazi hai or mein c language sekh raha hon 
+kiuk meine software engineering mein bachelor krha hon
+*/
+
+
+
+
+
+int main()
+{
+    int age = 20;
+
+    double name;
+    double father_name; 
+    double religion; 
+    double country; 
+    /*float pi = 3.14;
+
+    char operation = '+';*/
+
+
+printf("\nYour Name:\t");
+scanf("%s", &name);
+
+printf("\nFather Name:\t");
+scanf("%s", &father_name);
+
+printf("\nYour Age:\t");
+scanf("%s", &age);
+
+printf("\nReligion:\t");
+scanf("%s", &religion);
+
+printf("\nCountry name:\t");
+scanf("%s", &country);
+
+return 0;
+
+
+}
